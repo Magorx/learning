b873980ffa0afe21ffe307002d3c9b685433516b
Maxim Gorishniy <maxim.gorishniy@gmail.com>
2017-04-27 07:51:27 +0300

    everyday

--- python/mini-projects/easy_rpg/menu.py
+++ python/mini-projects/easy_rpg/menu.py
@@ -11,47 +11,67 @@ class Menu(object):
         if len(buttons) != len(links):
             raise IndexError
 
-        self.buttons = buttons
-        self.links = links
+        self.buttons = \
+            [['Back', 0]] + [[buttons[i], links[i]] for i in range(len(buttons))]
         self.parent = parent
         self.name = name
         self.id = id
         self.back_links = back_links
 
-    def print_buttons(self):
-        for i in range(len(self.buttons)):
-            print('{}. {}'.format(i, self.buttons[i]))
+        self.update_longest_button()
+
+    def update_longest_button(self):
+        max_len = 0
+        for button in self.buttons:
+                button_name = button[0]
+                try:
+                    button_name = str(button_name)
+                except Exception:
+                    continue
+                if len(button_name) > max_len:
+                    max_len = len(button_name)
+        self.max_button_name_len = max_len
+
+    def print_self(self):
+        for i in range(1, len(self.buttons)):
+            print('|{: ^{max}}|'.format('{}. {}'.format(i, self.buttons[i][0]), max=self.max_button_name_len+3))
 
     def button_pressed(self, button_index):
-        if self.links[button_index] in self.back_links:
-            return 'Back'
-        link = self.links[button_index]
+        if button_index == 0:
+            return None
+        link = self.buttons[button_index][1]
         if callable(link):
             link()
-        elif isinstance(link, Menu):
-            link.call()
+            return 0
 
-    def call(self):
+    def __call__(self):
         while True:
-            print('--{}--'.format(self.name))
-            self.print_buttons()
-            print('-' * (len(self.name) + 4))
+            print('_' * (self.max_button_name_len + 5))
+            self.print_self()
+            print('_' * (self.max_button_name_len + 5))
             pressed = input()
             try:
                 pressed = int(pressed)
             except Exception:
                 print('Input should be integer')
                 continue
-            if pressed >= len(self.links):
+            if pressed >= len(self.buttons) or pressed < 0:
                 print('Input must be chosen from variants')
                 continue
             to_do = self.button_pressed(pressed)
-            if to_do == 'Back':
+            if to_do is None:
                 return 0
 
 
 def main():
-    pass
+    def hi():
+        print('HELLO')
+    def bye():
+        print('GOODBYE')
+    third = Menu(['Bye', 'back'], [bye, 'back'])
+    second = Menu(['Hello', 'back'], [hi, 'back'])
+    one = Menu(['Hi_menu', 'Bye_menu', 'exit'], [second, third, exit])
+    one()
 
 
 if __name__ == "__main__":
